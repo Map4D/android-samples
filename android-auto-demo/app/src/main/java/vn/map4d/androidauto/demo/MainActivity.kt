dcacf70770114253f4a6a0a8384657af84e75e13
Duy Dung <dungvd@iotlink.com.vn>
2023-07-06 11:00:47 +0700

    Create Android Auto Demo project

---
+++ android-auto-demo/app/src/main/java/vn/map4d/androidauto/demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package vn.map4d.androidauto.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
