9aa393d22ef9ecf821f38da26e1911e0eb89b385
Duy Dung <dungvd@iotlink.com.vn>
2023-09-06 09:30:27 +0700

    Add Support Map Fragment Demo

---
+++ support-map-fragment-demo/app/src/main/java/vn/map4d/map/demo/MainActivity.kt
@@ -0,0 +1,36 @@
+package vn.map4d.map.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vn.map4d.map.annotations.MFMarkerOptions
+import vn.map4d.map.camera.MFCameraUpdateFactory
+import vn.map4d.map.core.MFSupportMapFragment
+import vn.map4d.map.core.Map4D
+import vn.map4d.map.core.OnMapReadyCallback
+import vn.map4d.map.demo.databinding.ActivityMainBinding
+import vn.map4d.types.MFLocationCoordinate
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+  private lateinit var binding: ActivityMainBinding
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    val mapFragment: MFSupportMapFragment? = supportFragmentManager
+      .findFragmentById(R.id.map) as MFSupportMapFragment?
+    mapFragment?.getMapAsync(this)
+  }
+
+  override fun onMapReady(map: Map4D?) {
+
+    // Add a marker in Sydney and move the camera
+    val danang = MFLocationCoordinate(16.075884, 108.228932)
+    map?.addMarker(MFMarkerOptions().position(danang).title("Marker in Da Nang"))
+    map?.moveCamera(MFCameraUpdateFactory.newCoordinate(danang))
+  }
+}
