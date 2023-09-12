0c4a92953165159c788d6f0baebedc8e934fcadd
Duy Dung <dungvd@iotlink.com.vn>
2023-09-12 11:01:34 +0700

    Update simple mapview project

--- simple-mapview-demo/app/src/main/java/vn/map4d/map/demo/MainActivity.kt
+++ simple-mapview-demo/app/src/main/java/vn/map4d/map/demo/MainActivity.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import vn.map4d.map.annotations.MFMarkerOptions
 import vn.map4d.map.camera.MFCameraUpdateFactory
-import vn.map4d.map.core.MFSupportMapFragment
 import vn.map4d.map.core.Map4D
 import vn.map4d.map.core.OnMapReadyCallback
 import vn.map4d.map.demo.databinding.ActivityMainBinding
 import vn.map4d.types.MFLocationCoordinate
+import vn.map4d.map.core.MFMapView
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
   private lateinit var binding: ActivityMainBinding
+
+  private var mapView: MFMapView? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
-    binding.map.getMapAsync(this)
+    mapView = binding.map
+    mapView?.onCreate(savedInstanceState)
+    mapView?.getMapAsync(this)
   }
 
   override fun onMapReady(map: Map4D?) {
@@ -26,5 +31,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     val danang = MFLocationCoordinate(16.075884, 108.228932)
     map?.addMarker(MFMarkerOptions().position(danang).title("Marker in Da Nang"))
     map?.moveCamera(MFCameraUpdateFactory.newCoordinate(danang))
+  }
+
+  override fun onDestroy() {
+    mapView?.onDestroy()
+    super.onDestroy()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView?.onSaveInstanceState(outState)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    mapView?.onStart()
+  }
+
+  override fun onStop() {
+    mapView?.onStop()
+    super.onStop()
   }
 }
